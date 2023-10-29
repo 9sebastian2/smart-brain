b495e1a2bd9cdd5aadeb673bc7b107c2102daf8e
Sebastian Kiolbassa <skiolbassa@Angelicas-MacBook-Pro.local>
2023-10-29 17:39:14 -0500

    Adding starting project

---
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ImageLinkForm = () => {
+    return (
+        <div>
+            <p className='f3'>
+                {'This magic brain will detect faces in pictures you upload'}
+            </p>
+            <input className='f4 pa2 w-70 center' type='text' />
+            <button>Detect</button>
+
+        </div>
+    );
+}
+
+export default ImageLinkForm;
