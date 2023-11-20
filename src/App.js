293d1eea76e471a81a069d7e8d29144b3ad5da5e
Sebastian Kiolbassa <9sebastian2@gmail.com>
2023-11-19 21:22:16 -0600

    added functionality to input and detect button to console log. Also fixed particles background.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { loadSlim } from "tsparticles-slim";
 import Navigation from './Components/Navigation/Navigation';
 import Particles from "react-tsparticles";
@@ -8,76 +8,88 @@ import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Rank from './Components/Rank/Rank';
 
 function App() {
-  const particlesInit = useCallback(async engine => {
-    console.log(engine);
-    await loadSlim(engine);
-  }, []);
+    const [input, setInput] = useState('')
 
-  const particlesLoaded = useCallback(async container => {
-      console.log(container);
-  }, []);
+    const particlesInit = useCallback(async engine => {
+        console.log(engine);
+        await loadSlim(engine);
+    }, []);
 
-  const particlesOptions = {
-    fpsLimit: 120,
-    interactivity: {
-        events: {
-            onClick: {
+    const particlesLoaded = useCallback(async container => {
+        console.log(container);
+    }, []);
+
+    const particlesOptions = {
+        fpsLimit: 120,
+        interactivity: {
+            events: {
+                // onClick: {
+                //     enable: true,
+                //     mode: "push",
+                // },
+                onHover: {
+                    enable: true,
+                    mode: "repulse",
+                },
+                resize: true,
+            },
+            modes: {
+                push: {
+                    quantity: 4,
+                },
+                repulse: {
+                    distance: 110,
+                    duration: 0.4,
+                },
+            },
+        },
+        particles: {
+            color: {
+                value: "#ffffff",
+            },
+            links: {
+                color: "#ffffff",
+                distance: 150,
                 enable: true,
-                mode: "push",
+                opacity: 0.5,
+                width: 1,
             },
-            onHover: {
+            move: {
+                direction: "none",
                 enable: true,
-                mode: "repulse",
+                outModes: {
+                    default: "bounce",
+                },
+                random: false,
+                speed: 2,
+                straight: false,
             },
-            resize: true,
-        },
-        modes: {
-            push: {
-                quantity: 4,
-            },
-            repulse: {
-                distance: 110,
-                duration: 0.4,
+            number: {
+                density: {
+                    enable: true,
+                    area: 800,
+                },
+                value: 175,
             },
         },
-    },
-    particles: {
-        color: {
-            value: "#ffffff",
-        },
-        links: {
-            color: "#ffffff",
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1,
-        },
-        move: {
-            direction: "none",
-            enable: true,
-            outModes: {
-                default: "bounce",
-            },
-            random: false,
-            speed: 2,
-            straight: false,
-        },
-        number: {
-            density: {
-                enable: true,
-                area: 800,
-            },
-            value: 175,
-        },
-    },
-    detectRetina: true,
-  }
+        detectRetina: true,
+    }
+
+    const onInputChange = (event) => {
+        setInput(event.target.value);
+        console.log(input);
+    }
+
+    const onButtonSubmit = () => {
+        console.log('click');
+    }
+
   return (
     <div className="App">
           <Navigation />
           <Logo />
           <Rank />
-          <ImageLinkForm />
+          <ImageLinkForm onInputChange={onInputChange} onButtonSubmit={onButtonSubmit}/>
           {/* 
           <FaceRecognition />} */}
       <Particles
